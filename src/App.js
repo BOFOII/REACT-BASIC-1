317d5fccb8acb843f3f5c73a339ae45339268b85
XarraXPMMP <imzalpha2@gmail.com>
2021-09-28 19:17:51 -0700

    2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MenuMakanan from './components/MenuMakanan/MenuMakanan.js';
-import MenuMinuman from './components/MenuMinuman/MenuMinuman.js'
+import MenuMinuman from './components/MenuMinuman/MenuMinuman.js';
 
 class App extends Component {
 
@@ -11,20 +11,18 @@ class App extends Component {
     super()
     this.state = {
       namaResto: 'ZET RESTO',
-      pelanggan: {
-        nama: 'riski',
-      },
+      namaPembeli: 'zalpha',
+      keterangan: '',
+      pembayaran: '',
       menuMakanan: [
-        {
-          nama: 'Ayam bakar',
-          harga: 25000,
-          stock: 10,
-        },
-        {
-          nama: 'Sate',
-          harga: 25000,
-          stock: 0,
-        },
+        { id: 1, nama: 'Ayam bakar', harga: 25000, stock: 10 },
+        { id: 2, nama: 'Sate', harga: 25000, stock: 0 },
+      ],
+      menuMinuman: [
+        { id: 1, nama: 'Jus teh', harga: 10000, stock: 10 },
+        { id: 2, nama: 'Jus Melon', harga: 10000, stock: 10 },
+        { id: 3, nama: 'Jus mangga', harga: 10000, stock: 10 },
+        { id: 4, nama: 'Jus alpukat', harga: 10000, stock: 0 },
       ]
     };
   }
@@ -34,17 +32,58 @@ class App extends Component {
     this.setState({ namaResto: nama_resto });
   }
 
+  //
+  gantiNamapembeli = (event) => {
+    this.setState({ namaPembeli: event.target.value });
+  }
+
+  //
+  keteranganHandler = (event) => {
+    this.setState({ keterangan: event.target.value });
+  }
+
+  //
+  pembayaranHandler = (event) => {
+    this.setState({ pembayaran: event.target.value });
+  }
+
   // render
   render() {
     return (
       <div className="App">
         <h1>CAFFE { this.state.namaResto }</h1>
         <button className="btn btn-primary" onClick={ () => this.gantiNamaHandler("ZET Resto") }>Ganti nama</button>
+        <div className="row mt-5" style={{ marginTop: '10px' }} >
+          <div className="col-md-4 offset-md-4" >
+            <form>
+              <div className="form-group">
+                <label>Nama pembeli: </label>
+                <input className="form-control" name="namaPembeli" onChange={ this.gantiNamapembeli } value={ this.state.namaPembeli }/>
+              </div>
+              <div className="form-group">
+                <label>Keterangan: </label>
+                <textarea className="form-control" name="keterangan" onChange={ this.keteranganHandler } value={ this.state.keterangan }></textarea>
+                <small className="mt-1">{ this.state.keterangan }</small>
+              </div>
+              <div className="form-group">
+                <label>Jenis pembayaran</label>
+                <select className="form-control" value={ this.state.pembayaran } onChange={ this.pembayaranHandler }>
+                  <option value="cash">Cash</option>
+                  <option value="ngutang">Ngutang</option>
+                  <option value="voucher" >Voucher</option>
+                </select>
+                <small className="mt-1">{ this.state.pembayaran }</small>
+              </div>
+            </form>
+          </div>
+        </div>
         <h2>Menu Makanan</h2>
         <MenuMakanan namaMenu={ this.state.menuMakanan[0].nama } harga={ this.state.menuMakanan[0].harga } stock={ this.state.menuMakanan[0].stock }></MenuMakanan>
         <MenuMakanan namaMenu={ this.state.menuMakanan[1].nama } harga={ this.state.menuMakanan[1].harga } stock={ this.state.menuMakanan[1].stock }></MenuMakanan>
         <h2>Menu Minuman</h2>
-        <MenuMinuman namaMenu="Jus Teh" harga={ 5000 }></MenuMinuman>
+        { this.state.menuMinuman.map(menu => 
+          <MenuMinuman key={ menu.id } namaMenu={ menu.nama } harga={ menu.harga } stock={ menu.stock }></MenuMinuman>
+          )}
       </div>
     );
   }
--- src/components/MenuMinuman/MenuMinuman.js
+++ src/components/MenuMinuman/MenuMinuman.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 
 class MenuMinuman extends Component {
     render() {
-        return(
-            <div style={{ border: "1px solid", width: 300, margin: "auto" }}>
-                <p>Nama Menu : { this.props.namaMenu }</p>
-                <p>Harga : Rp { this.props.harga }</p>
-            </div>
-        );
+        if(this.props.stock !== 0) {
+            return(
+                <div style={{ border: "1px solid", width: 300, margin: "auto" }}>
+                    <p>Nama Menu : { this.props.namaMenu }</p>
+                    <p>Harga : Rp { this.props.harga }</p>
+                </div>
+            );
+        } else {
+            return null;
+        }
     }
 }
 
